f1cee8027c0542d7522de95a561879fae4abb4a6
Ivelisya <138857469+Ivelisya@users.noreply.github.com>
2025-03-11 09:18:10 +0800

    1

--- day_2/HousePlateProduction.cpp
+++ day_2/HousePlateProduction.cpp
@@ -8,7 +8,17 @@
 
 #include <iostream>
 using namespace std;
+int two = 0;
 int main(){
-    
+    for(int i =1;i <= 2020; ++i){
+        int temp = i;
+        while(temp){
+            if(temp % 10 == 2){
+                two++;
+            }
+            temp /= 10;
+        }   
+    }
+    cout << two << endl;
     return 0;
 }
