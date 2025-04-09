783539985a5c53b509aab303c1befb15e8238cad
Ivelisya <138857469+Ivelisya@users.noreply.github.com>
2025-04-09 22:11:44 +0800

    Implement depth-first search for tree traversal with weighted nodes in test3.cpp

--- build/CMakeFiles/smzy.dir/cmake_clean.cmake
+++ build/CMakeFiles/smzy.dir/cmake_clean.cmake
@@ -1,6 +1,6 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/smzy.dir/day_12/test3.cpp.obj"
-  "CMakeFiles/smzy.dir/day_12/test3.cpp.obj.d"
+  "CMakeFiles/smzy.dir/day_20/test3.cpp.obj"
+  "CMakeFiles/smzy.dir/day_20/test3.cpp.obj.d"
   "libsmzy.dll.a"
   "smzy.exe"
   "smzy.exe.manifest"
