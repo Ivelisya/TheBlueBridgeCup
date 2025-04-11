6065eab8ecd294a5c5e70d98b6d056ae009c7ea5
Ivelisya <138857469+Ivelisya@users.noreply.github.com>
2025-04-11 17:08:37 +0800

    添加素数表生成和最大公约数计算功能，更新相关代码结构

--- day_22/test1.cpp
+++ day_22/test1.cpp
@@ -1,14 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int N = 1e7;
-vector<bool> is_prime(N, true);
+vector<bool> is_prime(N + 1, true);
 int main()
 {
+    // 创建素数表
+    int ans = 0;
+    int sum = 0;
     is_prime[1] = false;
     for (int i = 2; i <= N; i++)
     {
         if (is_prime[i])
         {
+            ans = i;
+            sum++;
+            if (sum == 1e5 + 2)
+                return cout << ans << endl, 0;
             if ((long long)i * i > N)
                 continue;
             for (int j = i * i; j <= N; j += i)
@@ -17,8 +24,9 @@ int main()
             }
         }
     }
+    cout << -1;
+#if 0
     int i = 1e5 + 2;
-    int ans = 0;
     for (int j = 1; j <= N; j++)
     {
 
@@ -34,4 +42,5 @@ int main()
         }
     }
     return cout << -1 << endl, 0;
+#endif
 }
